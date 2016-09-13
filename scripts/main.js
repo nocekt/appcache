ffd4f85ae91b019674d73abd089f68192a3183da
nocekt <nocekt@gmail.com>
2016-09-13 14:07:40 +0200

    First attempt.

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+var someHtml = "<font color='red'> b </font>" +
+"<font color='orange'> u </font>" +
+"<font color='blue'> r </font>" +
+"<font color='yellow'> r </font>" +
+"<font color='green'> i </font>" +
+"<font color='bink'> t </font>" +
+"<font color='white'> o </font>";
+
+var someImage = "<br><img src='./burrito.png'>";
+
+window.onload = function(e){
+	document.getElementById("b1").onclick = function() {
+		document.getElementById("d1").innerHTML += someHtml;
+		setTimeout(function(){
+			document.getElementById("d1").innerHTML += someImage; 
+		}, 1000);
+	}
+}
